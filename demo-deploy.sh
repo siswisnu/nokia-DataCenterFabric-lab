c8b1964c0d5d1eb3508b2323d2588d203bee3075
Your Name <you@example.com>
2023-10-20 16:47:21 +0000

    main

--- demo-deploy.sh
+++ demo-deploy.sh
@@ -15,5 +15,7 @@ docker pull ghcr.io/asadarafat/topoviewer:latest
 docker image prune -f
 
 wget --no-cache https://raw.githubusercontent.com/asadarafat/topoViewer/master/rawTopoFile/clab-topo-export-template-example/clab-topo-new-version-cytoscape.tmpl
+wget --no-cache https://raw.githubusercontent.com/asadarafat/nokia-DataCenterFabric-lab/main/3tierSmall.yaml
+
 
 clab deploy -t 3tierSmall.yaml --export-template clab-topo-new-version-cytoscape.tmpl
