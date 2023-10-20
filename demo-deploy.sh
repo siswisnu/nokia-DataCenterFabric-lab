8036baae3953986f52c32fa6ff490e4bfa682ca8
Your Name <you@example.com>
2023-10-20 15:52:44 +0000

    main

--- demo-deploy.sh
+++ demo-deploy.sh
@@ -1,45 +1,19 @@
 
 #!/bin/bash
 
-# export http_proxy=http://135.245.192.7:8000 && export https_proxy=http://135.245.192.7:8000
-# modprobe ip_tables && sudo echo 'ip_tables' >> /etc/modules 
 clab destroy -t 3tierSmall.yaml
-# rm *.yaml*
 rm *.tmpl*
 
+echo "create dummpy lincenses"
 mkdir license
 touch license/lic-srl.txt
-touch license/lic-sros.txt
+touch license/lic-sros.txt 
 
 mkdir startup-config
-# wget --no-cache -O startup-config/aggUPF-01.cfg https://raw.githubusercontent.com/asadarafat/nokia-MultiAccessGateway-lab/main/startup-config/aggUPF-01.cfg
-# wget --no-cache -O startup-config/aggUPF-02.cfg https://raw.githubusercontent.com/asadarafat/nokia-MultiAccessGateway-lab/main/startup-config/aggUPF-02.cfg
-# wget --no-cache -O startup-config/magC-01.cfg https://raw.githubusercontent.com/asadarafat/nokia-MultiAccessGateway-lab/main/startup-config/magC-01.cfg
-# wget --no-cache -O startup-config/magC-02.cfg https://raw.githubusercontent.com/asadarafat/nokia-MultiAccessGateway-lab/main/startup-config/magC-02.cfg
-# wget --no-cache -O startup-config/pe-01.cfg https://raw.githubusercontent.com/asadarafat/nokia-MultiAccessGateway-lab/main/startup-config/pe-01.cfg
-
 
 docker pull ghcr.io/asadarafat/topoviewer:latest
 docker image prune -f
 
 wget --no-cache https://raw.githubusercontent.com/asadarafat/topoViewer/master/rawTopoFile/clab-topo-export-template-example/clab-topo-new-version-cytoscape.tmpl
 
-# wget --no-cache https://raw.githubusercontent.com/asadarafat/nokia-MultiAccessGateway-lab/main/topo-nokia-MAG-lab.yaml
-
-# mkdir radius-config
-# wget --no-cache -O radius-config/clients.conf https://raw.githubusercontent.com/asadarafat/nokia-MultiAccessGateway-lab/main/radius-config/clients.conf
-# wget --no-cache -O radius-config/users https://raw.githubusercontent.com/asadarafat/nokia-MultiAccessGateway-lab/main/radius-config/users
-
 clab deploy -t 3tierSmall.yaml --export-template clab-topo-new-version-cytoscape.tmpl
-
-echo "Enter the following command to run topoviewer, which web-based GUI for containerlab:"
-echo "#############"
-echo "#############  docker exec -it clab-nokia-MAGc-lab-topoviewer /opt/topoviewer/topoviewer clab --allowed-hostnames 138.203.26.59 --server-port 8080 --clab-user root --clab-pass j0k0w1 --topology-file-json local-bind/topo-file.json"
-echo "#############"
-echo "         #### 'root' corresponds to the server username where containerLab is currently operational. "
-echo "         #### 'j0k0w1', corresponds to user's password"
-echo "         #### '138.203.26.59', corresponds to the server IP address where containerLab is currently operational."
-echo "         #### '8080', corresponds to the port where the topoViewer service will be listening."
-
-#
-#
